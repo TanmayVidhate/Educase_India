4558e3632101a89472ef8cc599572a13154a07bf
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-31 20:19:34 +0530

    add signinPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Routes,Route } from 'react-router-dom';
 //import views
 import LandingPage from './Views/LandingPage';
 import CreateAccount from './Views/CreateAccount';
+import SigninPage from './Views/SigninPage';
 
 function App() {
-  
+
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create_account" element={<CreateAccount />} />
+        <Route path="/signin" element={<SigninPage />} />
       </Routes>
     </>
   )

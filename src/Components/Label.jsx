815907c10e479c95fff8ca6b10b5026cd9207853
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-31 15:56:51 +0530

    add label

---
+++ src/Components/Label.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Label({ title }) {
+  
+  return (
+    <div className='w-full m-1 text-gray-700 font-semibold text-lg md:text-xl lg:text-base '>
+      {title}  
+    </div>
+  )
+}
+
+export default Label

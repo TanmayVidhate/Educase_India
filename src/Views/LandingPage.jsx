49b5c9b899c92ccbe9b6e043f56183643d2ec0af
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-31 14:11:08 +0530

    add div

--- src/Views/LandingPage.jsx
+++ src/Views/LandingPage.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
 function LandingPage() {
-  return (
-    <>
-        <div>
+    return (
+        <>
+            <div className='border-4 border-red-500 h-screen w-screen  flex justify-center items-center'>
 
-            <h2 className='border bg-red-500'>Welcome to PopX</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, voluptate?</p>
-        </div>
-    </>
-  )
+                <div className='border-4 border-red-500 h-[80vh] w-1/4 p-2'>
+                    <div className='border-4 border-red-500 h-2/3'>
+
+                    </div>
+                    <div className=''>
+                        <h2 className='text-3xl font-bold'>Welcome to PopX</h2>
+                    </div>
+                    <div className='my-1 text-gray-400'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default LandingPage

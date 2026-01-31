a02cbbd1a5e5b3bc3de6808181c58bcd755019a3
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-31 12:49:26 +0530

    remove import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 
 //import views
 import LandingPage from './Views/LandingPage';

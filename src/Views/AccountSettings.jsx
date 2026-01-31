28512ac20a2ea3890d22b3e9efa87e1991d5a9ae
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-31 23:59:00 +0530

    add code

--- src/Views/AccountSettings.jsx
+++ src/Views/AccountSettings.jsx
@@ -7,23 +7,36 @@ import InputField from '../Components/InputField'
 import Button from '../Components/Button';
 
 function AccountSettings() {
-  return (
-    <>  
-         <div className='h-screen w-screen  flex justify-center items-center '>
+    return (
+        <>
+            <div className='h-screen w-screen  flex justify-center items-center '>
 
                 <div className='border border-gray-100 min-h-min w-[350px] p-2 bg-[#f7f8f9]'>
                     <div className='pl-2 w-1/2 mt-5'>
                         <p className='text-xl font-medium '>Account Settings</p>
                     </div>
-                   
 
+                    <div className='mt-5 flex justify-start items-start gap-10'>
+                        <div className='h-20 w-20 border border-red-500 rounded-full'></div>
 
-                    
+                        <div className=''>
+                            <p className='text-sm font-bold'>Marry Doe</p>
+                            <p className='text-sm'>marray@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div className='mt-5' >
+                        <p className='pl-2 text-sm text-gray-400'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, eveniet. Incidunt aperiam quos rerum odio</p>
+                    </div>
+
+                    <div className='h-[50vh]'>
+
+                    </div>
                 </div>
 
             </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default AccountSettings

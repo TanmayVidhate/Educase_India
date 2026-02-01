b18a199f6ecd0ca1e4b15e7435939bbdd3436bb1
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-02-01 12:00:02 +0530

    install toast and use

--- src/Views/SigninPage.jsx
+++ src/Views/SigninPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 //import components
 import Label from '../Components/Label'
@@ -11,8 +12,7 @@ function SigninPage() {
 
     const navigate = useNavigate()
 
-    const {
-        register,
+    const { register,
         handleSubmit,
         formState: { errors },
         control,
@@ -22,11 +22,24 @@ function SigninPage() {
         mode: "onChange",
         defaultValues: {
             email_field: "",
-            pass_field:""
+            pass_field: ""
         },
     });
 
+    const email = watch("email_field");
+    const pass = watch("pass_field");
 
+    const signinPagess = (e) => {
+        e.preventDefault();
+
+        if (email.length === 0 || pass.length === 0) {
+            toast.error("Please Enter Email and Password..")
+        }
+        else {
+            console.log("all is ok...")
+            navigate("/account")
+        }
+    }
     return (
         <>
             <div className='h-screen w-screen  flex justify-center items-center '>
@@ -40,77 +53,79 @@ function SigninPage() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </div>
 
-                    <div className='my-1 text-gray-400 w-full pl-2 '>
-                        <div className=''>
-                            <div className='relative'>
-                                <Label title="Email Address" className="absolute  " />
-                                <InputField
-                                    className=""
-                                    name="email"
-                                    {...register("email_field", {
-                                        required: "Email is required",
-                                        pattern: {
-                                            value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-                                            message: "Enter a valid email",
-                                        },
-                                    })}
-                                />
-                                {errors?.email_field && (
-                                    <span className="text-red-500 text-x w-5/6 absolute left-2 -bottom-5 ">
-                                        {errors?.email_field?.message}
-                                    </span>
-                                )}
-                            </div>
+                    <form onSubmit={signinPagess}>
+                        <div className='my-1 text-gray-400 w-full pl-2 '>
+                            <div className=''>
+                                <div className='relative'>
+                                    <Label title="Email Address" className="absolute  " />
+                                    <InputField
+                                        className=""
+                                        name="email"
+                                        {...register("email_field", {
+                                            required: "Email is required",
+                                            pattern: {
+                                                value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+                                                message: "Enter a valid email",
+                                            },
+                                        })}
+                                    />
+                                    {errors?.email_field && (
+                                        <span className="text-red-500 text-x w-5/6 absolute left-2 -bottom-5 ">
+                                            {errors?.email_field?.message}
+                                        </span>
+                                    )}
+                                </div>
 
-                            <div className='relative mt-5'>
-                                <Label title="Password" className="absolute  " />
-                                <InputField
-                                    className=""
-                                    type="password"
-                                    name="pass_field"
-                                    {...register("pass_field", {
-                                        required: "Password is required.",
+                                <div className='relative mt-5'>
+                                    <Label title="Password" className="absolute  " />
+                                    <InputField
+                                        className=""
+                                        type="password"
+                                        name="pass_field"
+                                        {...register("pass_field", {
+                                            required: "Password is required.",
 
-                                        pattern: {
-                                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                                            message: "Invalid password format",
-                                        },
-                                    })}
-                                />
-                                {errors?.pass_field && (
-                                    <span className="text-red-500 text-x w-5/6 absolute left-2 -bottom-5 ">
-                                        {errors?.pass_field?.message}
-                                    </span>
-                                )}
+                                            pattern: {
+                                                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                                message: "Invalid password format",
+                                            },
+                                        })}
+                                    />
+                                    {errors?.pass_field && (
+                                        <span className="text-red-500 text-x w-5/6 absolute left-2 -bottom-5 ">
+                                            {errors?.pass_field?.message}
+                                        </span>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='w-full p-1   mt-10 mb-5 sm:mt-11 '>
-                        {
-                            (Object.entries(errors).length > 0) ? (
-                                <Button name="login"
-                                    disabled={true}
-                                    hover={true}
-                                    onClick={()=>{ navigate('/account')}}
-                                    className="!px-5 !py-2 !rounded-lg !w-full !bg-[#6c25ff]"
-                                />)
-                                : (
+                        <div className='w-full p-1   mt-10 mb-5 sm:mt-11 '>
+                            {
+                                (Object.entries(errors).length > 0) ? (
                                     <Button name="login"
-                                        disabled={false}
+                                        disabled={true}
                                         hover={true}
-                                         onClick={()=>{ navigate('/account')}}
+                                        // onClick={() => { signinPagess }}
                                         className="!px-5 !py-2 !rounded-lg !w-full !bg-[#6c25ff]"
-                                    />
-                                )
-                        }
-                    </div>
+                                    />)
+                                    : (
+                                        <Button name="login"
+                                            disabled={false}
+                                            hover={true}
+                                            // onClick={() => { signinPagess }}
+                                            className="!px-5 !py-2 !rounded-lg !w-full !bg-[#6c25ff]"
+                                        />
+                                    )
+                            }
+                        </div>
 
-                    <div className='h-[30vh]'>
+                        <div className='h-[30vh]'>
 
-                    </div>
+                        </div>
+                    </form>
                 </div>
-
+                <ToastContainer/>
             </div>
         </>
     )
